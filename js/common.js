e729285bfb1806ec4900dd250d160dc2539b25d5
qwer2711 <pjy940222@gmail.com>
2024-06-24 18:01:19 +0900

    240624

--- js/common.js
+++ js/common.js
@@ -9,77 +9,47 @@
 var common = (function () {
   return {
     init: function () {
-      common.sideMenu();
-      common.guideListLength();
-      common.searchText();
+      // if (document.getElementById("sideMenu")) {
+      //   common.sideMenu();
+      // }
+      if (document.querySelectorAll("area-tit")) {
+        common.guideListLength();
+      }
+      if (document.getElementById("searchText")) {
+        common.searchText();
+      }
     },
     // 서브페이지 사이드메뉴
-    sideMenu: function () {
-      const accordionHeaders = document.querySelectorAll(".accordion-header");
-      const subAccordionHeaders = document.querySelectorAll(
-        ".sub-accordion-header"
-      );
+    // sideMenu: function () {
+    //   const accordionHeaders = document.querySelectorAll(".accordion-header");
 
-      accordionHeaders.forEach((header) => {
-        header.addEventListener("click", function () {
-          const accordionItem = this.parentNode;
-          const content = accordionItem.querySelector(".accordion-content");
+    //   accordionHeaders.forEach((header) => {
+    //     header.addEventListener("click", function () {
+    //       const accordionItem = this.parentNode;
+    //       const content = accordionItem.querySelector(".accordion-content");
 
-          if (!accordionItem.classList.contains("open")) {
-            closeAllAccordions();
-            accordionItem.classList.add("open");
-            // content.style.maxHeight = "100%";
-            content.style.maxHeight = content.scrollHeight + "px";
-          } else {
-            closeAllAccordions();
-            accordionItem.classList.remove("open");
-            content.style.maxHeight = "0";
-          }
-        });
-      });
+    //       if (!accordionItem.classList.contains("open")) {
+    //         closeAllAccordions();
+    //         accordionItem.classList.add("open");
+    //         // content.style.maxHeight = "100%";
+    //         content.style.maxHeight = content.scrollHeight + "px";
+    //       } else {
+    //         closeAllAccordions();
+    //         accordionItem.classList.remove("open");
+    //         content.style.maxHeight = "0";
+    //       }
+    //     });
+    //   });
 
-      subAccordionHeaders.forEach((header) => {
-        header.addEventListener("click", function () {
-          const subAccordionItem = this.parentNode;
-          const content = subAccordionItem.querySelector(
-            ".sub-accordion-content"
-          );
-          const closest = subAccordionItem.closest(".accordion-content");
-          closest.style.maxHeight = "100%";
-
-          if (!subAccordionItem.classList.contains("open")) {
-            closeAllSubAccordions();
-            subAccordionItem.classList.add("open");
-            content.style.maxHeight = content.scrollHeight + "px";
-          } else {
-            subAccordionItem.classList.remove("open");
-            content.style.maxHeight = "0";
-          }
-        });
-      });
-
-      function closeAllAccordions() {
-        accordionHeaders.forEach((header) => {
-          const accordionItem = header.parentNode;
-          const content = accordionItem.querySelector(".accordion-content");
-          accordionItem.classList.remove("open");
-          content.style.maxHeight = "0";
-        });
-
-        closeAllSubAccordions();
-      }
-
-      function closeAllSubAccordions() {
-        subAccordionHeaders.forEach((header) => {
-          const subAccordionItem = header.parentNode;
-          const content = subAccordionItem.querySelector(
-            ".sub-accordion-content"
-          );
-          subAccordionItem.classList.remove("open");
-          content.style.maxHeight = "0";
-        });
-      }
-    },
+    //   function closeAllAccordions() {
+    //     accordionHeaders.forEach((header) => {
+    //       const accordionItem = header.parentNode;
+    //       const content = accordionItem.querySelector(".accordion-content");
+    //       accordionItem.classList.remove("open");
+    //       content.style.maxHeight = "0";
+    //     });
+    //   }
+    // },
     // 가이드 개수표시
     guideListLength: function () {
       const styLength = document.querySelectorAll(".sty-guide > li");
@@ -90,32 +60,48 @@ var common = (function () {
       const styNum = styLength.length;
       const comNum = comLength.length;
 
-      styInputNum.innerText = styNum;
-      comInputNum.innerText = comNum;
+      if (styInputNum) {
+        styInputNum.innerText = styNum;
+      }
+
+      if (comInputNum) {
+        comInputNum.innerText = comNum;
+      }
     },
-    // 가이드검색(1)
+    // 가이드검색창
     searchText: function () {
-      document
-        .getElementById("searchText")
-        .addEventListener("input", common.filter);
+      const searchTextInput = document.getElementById("searchText");
+      if (searchTextInput) {
+        searchTextInput.addEventListener("input", common.filter);
+      }
     },
-    // 가이드검색(2)
+    // 가이드검색필터
     filter: function () {
-      let search = document.getElementById("searchText").value;
+      let search = document.getElementById("searchText").value.trim();
       let listInner = document.querySelectorAll(".compo-guide li");
+      let resultsCount1 = 0;
 
       for (let i = 0; i < listInner.length; i++) {
         let city = listInner[i].getElementsByClassName("tit");
-        // let country = listInner[i].getElementsByClassName("desc");
-        if (
-          city[0].innerHTML.indexOf(search) != -1
-          // country[0].innerHTML.indexOf(search) != -1
-        ) {
-          console.log(city[0].innerHTML.indexOf(search));
+        if (city[0].innerHTML.indexOf(search) != -1) {
           listInner[i].style.display = "block";
+          resultsCount1++;
         } else {
           listInner[i].style.display = "none";
         }
+      }
+      // 검색결과 메시지 표시
+      const searchResultsMessage = document.getElementById(
+        "searchResultsMessage"
+      );
+      const comLength = document.querySelector("#comLength");
+
+      if (resultsCount1 === 0) {
+        searchResultsMessage.classList.replace("hide", "show");
+        comLength.innerText = `0`;
+      } else {
+        searchResultsMessage.classList.replace("show", "hide");
+        comLength.innerText = `${resultsCount1}`;
       }
     },
   };
