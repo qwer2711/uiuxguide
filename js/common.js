280353c03af497ec978724ed8a3a711e6246f515
qwer2711 <pjy940222@gmail.com>
2024-07-03 17:38:15 +0900

    끝

--- js/common.js
+++ js/common.js
@@ -6,6 +6,17 @@
  * 최종업데이트 : 2024-03-04
  ******************************************************** */
 
+/* ** 윈도우 사이즈 체크 (반응형) ** */
+let winSize;
+// function winSizeSet() {
+//   const brekpoint = 1024;
+//   if (window.innerWidth >= brekpoint) {
+//     winSize = "pc";
+//   } else {
+//     winSize = "mob";
+//   }
+// }
+
 // 공통JS
 var common = (function () {
   return {
@@ -15,6 +26,7 @@ var common = (function () {
         common.calender();
         common.disclosure();
         common.tab();
+        common.calenderInput();
       }
     },
     sideMenu: function () {
@@ -188,6 +200,66 @@ var common = (function () {
         });
       }
       layerTab();
+    },
+    calenderInput: function () {
+      $(function () {
+        $("#datepicker").datepicker({
+          dateFormat: "yy-mm-dd", //달력 날짜 형태
+          showOtherMonths: true, //빈 공간에 현재월의 앞뒤월의 날짜를 표시
+          showMonthAfterYear: true, // 월- 년 순서가아닌 년도 - 월 순서
+          changeYear: true, //option값 년 선택 가능
+          changeMonth: true, //option값  월 선택 가능
+          showOn: "both", //button:버튼을 표시하고,버튼을 눌러야만 달력 표시 ^ both:버튼을 표시하고,버튼을 누르거나 input을 클릭하면 달력 표시
+          buttonImage:
+            "http://jqueryui.com/resources/demos/datepicker/images/calendar.gif", //버튼 이미지 경로
+          buttonImageOnly: true, //버튼 이미지만 깔끔하게 보이게함
+          buttonText: "선택", //버튼 호버 텍스트
+          yearSuffix: "년", //달력의 년도 부분 뒤 텍스트
+          monthNamesShort: [
+            "1월",
+            "2월",
+            "3월",
+            "4월",
+            "5월",
+            "6월",
+            "7월",
+            "8월",
+            "9월",
+            "10월",
+            "11월",
+            "12월",
+          ], //달력의 월 부분 텍스트
+          monthNames: [
+            "1월",
+            "2월",
+            "3월",
+            "4월",
+            "5월",
+            "6월",
+            "7월",
+            "8월",
+            "9월",
+            "10월",
+            "11월",
+            "12월",
+          ], //달력의 월 부분 Tooltip
+          dayNamesMin: ["일", "월", "화", "수", "목", "금", "토"], //달력의 요일 텍스트
+          dayNames: [
+            "일요일",
+            "월요일",
+            "화요일",
+            "수요일",
+            "목요일",
+            "금요일",
+            "토요일",
+          ], //달력의 요일 Tooltip
+          minDate: "-5Y", //최소 선택일자(-1D:하루전, -1M:한달전, -1Y:일년전)
+          maxDate: "+5y", //최대 선택일자(+1D:하루후, -1M:한달후, -1Y:일년후)
+        });
+
+        //초기값을 오늘 날짜로 설정해줘야 합니다.
+        $("#datepicker").datepicker("setDate", "today"); //(-1D:하루전, -1M:한달전, -1Y:일년전), (+1D:하루후, -1M:한달후, -1Y:일년후)
+      });
     },
   };
 })();
@@ -389,9 +461,82 @@ const kds_swiper = {
   },
 };
 
+/*** * tooltip * ***/
+const krds_tooltip = {
+  init: () => {
+    krds_tooltip.tooltipEvent();
+  },
+  tooltipEvent: () => {
+    const _toolBtns = document.querySelectorAll(".krds-tooltip-wrap .tool-btn");
+
+    _toolBtns.forEach(($toolBtn) => {
+      const _span = document.createElement("span");
+      const _txt = document.createTextNode("열기");
+
+      _span.classList.add("sr-only");
+      _span.appendChild(_txt);
+
+      $toolBtn.innerHTML = "";
+      $toolBtn.appendChild(_span);
+
+      $toolBtn.addEventListener("click", ($el) => {
+        const $parent = $toolBtn.closest(".krds-tooltip-wrap");
+        const $closeBtn = $parent.querySelector(".tool-close");
+        const $cnt = $parent.querySelector(".tool-in");
+        const $srTxt = $el.target.querySelector(".sr-only");
+        if ($cnt.style.display !== "block") {
+          $cnt.style.display = "block";
+          $cnt.setAttribute("tabindex", 0);
+          $srTxt.textContent = "닫기";
+          krds_tooltip.tooltipResize($parent, $cnt);
+        }
+        $closeBtn.addEventListener("click", () => {
+          $srTxt.textContent = "열기";
+          $cnt.style.display = "";
+          $cnt.removeAttribute("tabindex");
+          $toolBtn.focus();
+          krds_tooltip.tooltipResize($parent, $cnt);
+        });
+
+        window.addEventListener("resize", () => {
+          krds_tooltip.tooltipResize($parent, $cnt);
+        });
+      });
+    });
+  },
+  tooltipResize: ($parent, $cnt) => {
+    if (winSize === "mob") {
+      krds_tooltip.tooltipMob($parent, $cnt);
+    } else {
+      krds_tooltip.tooltipPc($cnt);
+    }
+    window.addEventListener("resize", () => {
+      if (winSize === "mob") {
+        krds_tooltip.tooltipMob($cnt);
+      } else {
+        krds_tooltip.tooltipPc($cnt);
+      }
+    });
+  },
+  tooltipMob: ($parent, $cnt) => {
+    const _offsetL = $parent.offsetLeft - 16;
+    const _width = document.body.clientWidth;
+    const _offsetR = _width - ($parent.clientWidth + _offsetL) - 32;
+    if ($cnt) {
+      $cnt.style.left = `-${_offsetL}px`;
+      $cnt.style.right = `-${_offsetR}px`;
+    }
+  },
+  tooltipPc: ($cnt) => {
+    $cnt.style.left = "";
+    $cnt.style.right = "";
+  },
+};
+
 window.addEventListener("DOMContentLoaded", function () {
   common.init();
   kds_modal.init();
   kds_accordion.init();
   kds_swiper.init();
+  krds_tooltip.init();
 });
